5c4ca85d28d08cc1c9962fc7cc7091ff2938d2e3
Divya <divyamaurya025@gmail.com>
2024-08-01 22:23:04 +0530

    TextToSpeech_Dropdown with multiple voices

--- src/components/InputComp.jsx
+++ src/components/InputComp.jsx
@@ -3,8 +3,19 @@ import { useSpeechSynthesis } from "react-speech-kit";
 import "./InputComp.css";
 
 const InputComp = () => {
-  const { speak } = useSpeechSynthesis();
+  const { speak, voices, rate } = useSpeechSynthesis();
   const [value, setValue] = useState("");
+  const [voiceSelection, setVoiceSelecetion] = useState("");
+  const [speedSelection, setSpeedSelection] = useState();
+
+  const speeds = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2];
+  const handleSpeech = () => {
+    console.log("rate", rate);
+    speak({
+      text: value,
+      voice: voices.find((voice) => voice.name === voiceSelection),
+    });
+  };
 
   return (
     <div className="container">
@@ -15,7 +26,33 @@ const InputComp = () => {
         onChange={(e) => setValue(e.target.value)}
       />
       <br />
-      <button onClick={() => speak({ text: value })}>Generate Speech</button>
+      <select
+        onChange={(e) => setVoiceSelecetion(e.target.value)}
+        value={voiceSelection}
+      >
+        <option value="">Default</option>
+        {voices.map((voice, index) => {
+          return (
+            <option key={voice.name} value={index}>
+              {voice.name}
+            </option>
+          );
+        })}
+      </select>
+      <select
+        onChange={(e) => setSpeedSelection(e.target.value)}
+        value={speedSelection}
+      >
+        <option value="">1</option>
+        {speeds.map((speed, index) => {
+          return (
+            <option key={speed.rate} value={index}>
+              {speed.rate}
+            </option>
+          );
+        })}
+      </select>
+      <button onClick={handleSpeech}>Generate Speech</button>
     </div>
   );
 };
